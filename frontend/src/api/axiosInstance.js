9dd8194769bf69486c22a42d37391d60341eba28
purushotham-07 <purushothamreddy9440@gmail.com>
2025-11-28 12:16:49 +0530

    Render deploy config

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: "https://employee-tracker-backend.onrender.com/api",
 });
+
 
 // Attach JWT token automatically
 API.interceptors.request.use(
